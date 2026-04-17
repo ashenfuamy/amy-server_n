266dd1bba070d341edc0aa48e2ee3567b4748fab
Liu Yuanshen <52831297+ashenfuamy@users.noreply.github.com>
2026-04-17 17:03:04 +0800

    master:deving

--- amy-app/src/main/java/site/ashenstation/app/service/SerialService.java
+++ amy-app/src/main/java/site/ashenstation/app/service/SerialService.java
@@ -2,8 +2,16 @@ package site.ashenstation.app.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.ashenstation.annotation.UpdateResourceCache;
+import site.ashenstation.app.dto.CreateSerialDto;
 
 @Service
 @RequiredArgsConstructor
 public class SerialService {
+
+    @UpdateResourceCache
+    @Transactional
+    public void createSerial(CreateSerialDto dto) {
+    }
 }
